3c01dccac42c206b954af28ab17be673b36b1b17
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-12-25 00:49:56 +0100

    Source code

---
+++ script.js
@@ -0,0 +1,14 @@
+const searchBox = document.getElementById('search')
+
+searchBox.addEventListener('keydown', (e) => {
+  if (e.code == 'Enter') {
+    const { value } = searchBox
+    if (value.replace(/ /gm, '').length > 0) {
+      location.href = `https://duckduckgo.com/?q=${value}`
+    }
+  }
+})
+
+window.onload = () => {
+  document.getElementsByTagName('body')[0].removeAttribute('class')
+}
