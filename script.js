584576d56a9ac6b958bfc8960e310d88b67f4f71
Nimplex <39964594+Nimplex@users.noreply.github.com>
2023-05-05 09:50:40 +0200

    refactor

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const searchBox = document.getElementById('search')
+const searchBox = document.getElementById("search");
 
-searchBox.addEventListener('keydown', (e) => {
-  if (e.code == 'Enter') {
-    const { value } = searchBox
-    if (value.replace(/ /gm, '').length > 0) location.href = `https://duckduckgo.com/?q=${value}`
-    else searchBox.value = ''
-  }
-})
+searchBox.addEventListener("keydown", (e) => {
+	if (e.code == "Enter") {
+		const { value } = searchBox;
+		if (value.replace(/ /gm, "").length > 0) location.href = `https://duckduckgo.com/?q=${value}`;
+		else searchBox.value = "";
+	}
+});
 
 window.onload = () => {
-  document.getElementsByTagName('body')[0].removeAttribute('class')
-}
+	document.getElementsByTagName("body")[0].removeAttribute("class");
+};
